e96d4b29220325ee2caede60d558683e7961fe8f
leng <lyf@neuron.sg>
2019-11-28 18:14:04 +0800

    Initial Commit

---
+++ TopDock/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TopDock
+//
+//  Created by leng on 2019/11/28.
+//  Copyright © 2019 leng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
